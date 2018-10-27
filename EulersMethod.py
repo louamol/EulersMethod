6b60c2f0da091cfca7c0e5f7fc7bb2a8fa51facc
louamol <louan.mol@outlook.com>
2018-10-27 18:59:58 +0200

    Première version

---
+++ EulersMethod.py
@@ -0,0 +1,43 @@
+import numpy
+from matplotlib import pyplot
+
+# Create the time grid.
+T = 100.0  # length of the time-interval
+dt = 0.02  # time-step size
+N = int(T / dt) + 1  # number of time steps
+t = numpy.linspace(0.0, T, num=N)  # time grid
+
+# Set the initial conditions.
+z0 = 100.0  # altitude
+b0 = 10.0  # upward velocity resulting from gust
+zt = 100.0  # trim altitude
+g = 9.81  # acceleration due to gravity
+
+# Set the initial value of the numerical solution.
+u = numpy.array([z0, b0])
+
+# Create an array to store the elevation value at each time step.
+z = numpy.zeros(N)
+z[0] = z0
+
+# Temporal integration using Euler's method.
+for n in range(1, N):
+    rhs = numpy.array([u[1], g * (1 - u[0] / zt)]) #tableau avec (b_n, )
+    u = u + dt * rhs
+    z[n] = u[0]
+
+z_exact = (b0 * (zt / g)**0.5 * numpy.sin((g / zt)**0.5 * t) +
+           (z0 - zt) * numpy.cos((g / zt)**0.5 * t) + zt)
+# Plot the numerical solution and the exact solution.
+pyplot.figure(figsize=(9.0, 4.0))  # set the size of the figure
+pyplot.title('Elevation of the phugoid over the time')  # set the title
+pyplot.xlabel('Time [s]')  # set the x-axis label
+pyplot.ylabel('Elevation [m]')  # set the y-axis label
+pyplot.xlim(t[0], t[-1])  # set the x-axis limits
+pyplot.ylim(40.0, 160.0)  # set the y-axis limits
+pyplot.grid()  # set a background grid to improve readability
+pyplot.plot(t, z, label='Numerical',
+            color='C0', linestyle='-', linewidth=2)
+pyplot.plot(t, z_exact, label='Analytical',
+            color='C1', linestyle='-', linewidth=2)
+pyplot.legend();  # set the legend
